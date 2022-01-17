c516f612b78689160757a7d4e36a614e0fa1af34
Nemesislaw <narayangupta619@gmail.com>
2022-01-17 10:55:51 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:insta_clone/responsive/mobile_screen_layout.dart';
+import 'package:insta_clone/responsive/responsive_layout_screen.dart';
+import 'package:insta_clone/responsive/web_screen_layout.dart';
+import 'package:insta_clone/screens/login_screen.dart';
+import 'package:insta_clone/screens/signup_screen.dart';
+import 'package:insta_clone/utils/colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyDTqvU3YYHNY29fSkW1B9DFMt9uIf8NV-Y",
+            appId: "1:756061261961:web:20dd342323e51695823658",
+            messagingSenderId: "756061261961",
+            projectId: "insta-clone-4c01a",
+            storageBucket: "insta-clone-4c01a.appspot.com"));
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Insta Clone',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
+      // home: const ResponsiveLayout(
+      //   mobileScreenLayout: MobileScreenLayout(),
+      //   webScreenLayout: WebScreenLayout(),
+      // ),
+      home: const SignupScreen(),
+      // const Scaffold(
+      //   body: Text('Start Page'),
+      // ),
+    );
+  }
+}
